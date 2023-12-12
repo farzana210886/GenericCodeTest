a1d306f96257bf1e27ec2fb5fd6b6d2b37d2184d
HP <HP@DESKTOP-MC3H9LG>
2023-12-11 22:02:05 -0600

    Add project files.

---
+++ GenericValueCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCodeTest
+{
+    public class GenericValueCollection<T> where T : struct
+    {
+        private List<T> items;
+
+        public GenericValueCollection()
+        {
+            items = new List<T>();
+        }
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+        }
+
+        public T GetItem(int index)
+        {
+            if (index >= 0 && index < items.Count)
+            {
+                return items[index];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+        }
+
+        public List<T> GetSortedDescendingCollection()
+        {
+            List<T> sortedList = new List<T>(items);
+            sortedList.Sort();
+            sortedList.Reverse();
+            return sortedList;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericCodeTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericValueCollection<int> genericValueCollection = new GenericValueCollection<int>();
+
+            genericValueCollection.AddItem(10);
+            genericValueCollection.AddItem(5);
+            genericValueCollection.AddItem(13);
+            genericValueCollection.AddItem(3);
+
+
+            int item = genericValueCollection.GetItem(3);
+
+            Console.WriteLine($"\nItem at index 3: {item}");
+
+            Console.WriteLine("\nSorted Descending Collections are Below:");
+
+            CollectionOutput(genericValueCollection.GetSortedDescendingCollection());
+               
+        }
+
+       public static void CollectionOutput<T>(List<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+    }
+}
